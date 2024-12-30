3d91fc6af01f61193299c86d23e128d307476783
Kanika Kalia <kanika@bhangar.net>
2024-12-30 20:29:04 +0545

    create inventories

---
+++ src/app/api/enventories/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/app/lib/db/db";
+import { inventories } from "@/app/lib/db/schema";
+import { inventorySchema } from "@/app/lib/validators/inventorySchema";
+
+export async function POST(request: Request) {
+    const requestData = await request.json();
+    let validatedData;
+    try {
+        validatedData = await inventorySchema.parse(requestData);
+    } catch (err) {
+        return Response.json({ message: err }, { status: 400 });
+    }
+    try {
+        await db.insert(inventories).values(validatedData);
+        return Response.json({ message: 'OK' }, { status: 201 });
+    } catch (err) {
+        return Response.json(
+            { message: 'Failed to store the inventory into the database' },
+            { status: 500 }
+        );
+    }
+}

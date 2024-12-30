9ae1c89db3e574031796d654043a390fbbf1acfd
Kanika Kalia <kanika@bhangar.net>
2024-12-30 19:55:48 +0545

    create delivery persion endpoints

---
+++ src/app/api/delivery-persons/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/app/lib/db/db";
+import { deliveryPersons } from "@/app/lib/db/schema";
+import { deliveryPersonSchema } from "@/app/lib/validators/deliveryPersonSchema";
+
+export async function POST(request: Request) {
+    const requestData = await request.json();
+    let validatedData;
+    try {
+        validatedData = await deliveryPersonSchema.parse(requestData);
+    } catch (err) {
+        return Response.json({ message: err }, { status: 400 });
+    }
+    try {
+        await db.insert(deliveryPersons).values(validatedData);
+        return Response.json({ message: 'OK' }, { status: 201 });
+    } catch (err) {
+        return Response.json(
+            { message: 'Failed to store the delivery person into the database' },
+            { status: 500 }
+        );
+    }
+}

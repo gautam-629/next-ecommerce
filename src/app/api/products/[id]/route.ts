80a39f216533ead344ca1cf0ead90e47c16255a9
Kanika Kalia <kanika@bhangar.net>
2024-12-30 19:36:29 +0545

    add single product

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,19 @@
+import { db } from "@/app/lib/db/db";
+import { products } from "@/app/lib/db/schema";
+import { eq } from "drizzle-orm";
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const id = params.id;
+    try {
+        const product = await db
+            .select()
+            .from(products)
+            .where(eq(products.id, Number(id)))
+            .limit(1);
+        if (!product.length) {
+            return Response.json({ message: 'Product not found.' }, { status: 400 });
+        }
+        return Response.json(product[0]);
+    } catch (err) {
+        return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
+    }
+}

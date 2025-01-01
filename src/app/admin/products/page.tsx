84e133284b441da78f4b844099b3579ede71db9e
Kanika Kalia <kanika@bhangar.net>
2025-01-01 19:40:19 +0545

    add product

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -6,7 +6,11 @@ import { columns } from "./columns";
 import { useQuery } from "@tanstack/react-query";
 import { getAllProducts } from "@/http/api";
 import { Product } from "@/types";
+import { useNewProduct } from "@/store/product-store";
+import ProductSheet from "./product-sheet";
+
 const ProductsPage = () => {
+  const { onOpen } = useNewProduct();
   const { data: products } = useQuery<Product[]>({
     queryKey: ["products"],
     queryFn: getAllProducts,
@@ -16,7 +20,10 @@ const ProductsPage = () => {
     <>
       <div className="flex items-center justify-between">
         <h3 className="text-2xl font-bold tracking-tight">Products</h3>
-        <Button size={"sm"}>Add Product</Button>
+        <Button onClick={onOpen} size={"sm"}>
+          Add Product
+        </Button>
+        <ProductSheet />
       </div>
       <DataTable columns={columns} data={products || []} />
     </>
--- src/http/api.ts
+++ src/http/api.ts
@@ -3,3 +3,12 @@ export const getAllProducts = async () => {
     const response = await api.get('/products');
     return await response.data;
 };
+
+export const createProduct = async (data: FormData) => {
+    const response = await api.post('/products', data, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+    return response.data;
+};

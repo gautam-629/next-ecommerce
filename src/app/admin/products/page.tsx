1506074b4194d0b164d57d88e41b1ce7c2b1b5f1
Kanika Kalia <kanika@bhangar.net>
2025-01-01 20:03:33 +0545

    add loader spiner

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -8,10 +8,11 @@ import { getAllProducts } from "@/http/api";
 import { Product } from "@/types";
 import { useNewProduct } from "@/store/product-store";
 import ProductSheet from "./product-sheet";
+import { Loader2 } from "lucide-react";
 
 const ProductsPage = () => {
   const { onOpen } = useNewProduct();
-  const { data: products } = useQuery<Product[]>({
+  const { data: products, isLoading } = useQuery<Product[]>({
     queryKey: ["products"],
     queryFn: getAllProducts,
   });
@@ -25,7 +26,13 @@ const ProductsPage = () => {
         </Button>
         <ProductSheet />
       </div>
-      <DataTable columns={columns} data={products || []} />
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <Loader2 className="size-10 animate-spin" />
+        </div>
+      ) : (
+        <DataTable columns={columns} data={products || []} />
+      )}
     </>
   );
 };
